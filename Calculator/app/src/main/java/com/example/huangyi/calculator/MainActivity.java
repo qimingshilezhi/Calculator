5a0169b7b68ae2612b065b4b89be6a6db7fd760f
qimingshilezhi <30408526+qimingshilezhi@users.noreply.github.com>
2018-04-26 10:45:47 +0800

    3

--- Calculator/app/src/main/java/com/example/huangyi/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/huangyi/calculator/MainActivity.java
@@ -1,9 +1,21 @@
 package com.example.huangyi.calculator;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import java.text.DecimalFormat;
+import java.util.StringTokenizer;
 
-public class MainActivity extends AppCompatActivity {
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
